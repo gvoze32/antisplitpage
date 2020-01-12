c869aec807e3fea0cab0289cc91f68badb6a9fed
Syafa Adena <gvoze32@gmail.com>
2020-01-13 05:03:32 +0700

    simplify code

--- antisplitpage.user.js
+++ antisplitpage.user.js
@@ -11,34 +11,35 @@
 // @match        *://*.detik.com/*
 // @run-at       document-start
 // ==/UserScript==
-var stringPathName = window.location.href.toString()
 
-if (stringPathName.indexOf("tribun") != -1) {
-    if (window.location.href.toString().indexOf("?page=all") != -1) return false;
-    var pathLama = window.location.pathname;
-    if (!/\?page=all$/.test(pathLama)) {
-    var pathBaru = pathLama + "?page=all";
-    window.location.replace(pathBaru);
+var urlName = window.location.href.toString()
+
+if (urlName.indexOf("tribun") != -1) {
+    if (urlName.indexOf("?page=all") != -1) return false;
+    var oldPath = window.location.pathname;
+    if (!/\?page=all$/.test(oldPath)) {
+    var newPath = oldPath + "?page=all";
+    window.location.replace(newPath);
     }
- } else if (stringPathName.indexOf("detik") != -1) {
-    if (window.location.href.toString().indexOf("?single=1") != -1) return false;
-    var pathLamadua = window.location.pathname;
-    if (!/\?single=1$/.test(pathLamadua)) {
-    var pathBarudua = pathLamadua + "?single=1";
-    window.location.replace(pathBarudua);
+} else if (urlName.indexOf("grid") != -1) {
+    if (urlName.indexOf("?page=all") != -1) return false;
+    var oldPath2 = window.location.pathname;
+    if (!/\?page=all$/.test(oldPath2)) {
+    var newPath2 = oldPath2 + "?page=all";
+    window.location.replace(newPath2);
     }
-} else if (stringPathName.indexOf("grid") != -1) {
-    if (window.location.href.toString().indexOf("?page=all") != -1) return false;
-    var pathLamatiga = window.location.pathname;
-    if (!/\?page=all$/.test(pathLamatiga)) {
-    var pathBarutiga = pathLamatiga + "?page=all";
-    window.location.replace(pathBarutiga);
+} else if (urlName.indexOf("kompas") != -1) {
+    if (urlName.indexOf("?page=all") != -1) return false;
+    var oldPath3 = window.location.pathname;
+    if (!/\?page=all$/.test(oldPath3)) {
+    var newPath3 = oldPath3 + "?page=all";
+    window.location.replace(newPath3);
     }
-} else if (stringPathName.indexOf("kompas") != -1) {
-    if (window.location.href.toString().indexOf("?page=all") != -1) return false;
-    var pathLamaempat = window.location.pathname;
-    if (!/\?page=all$/.test(pathLamaempat)) {
-    var pathBaruempat = pathLamaempat + "?page=all";
-    window.location.replace(pathBaruempat);
-    }
+} else if (urlName.indexOf("detik") != -1) {
+    if (urlName.indexOf("?single=1") != -1) return false;
+    var oldPath4 = window.location.pathname;
+    if (!/\?single=1$/.test(oldPath4)) {
+    var newPath4 = oldPath4 + "?single=1";
+    window.location.replace(newPath4);
+	}
 }
